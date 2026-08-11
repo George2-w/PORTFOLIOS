415f34e867f3e26e9625efab0026617fbc0ac5ad
george ajileye <ajieyegeorge@gmail.com>
2026-08-11 00:40:44 -0600

    removed dot

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <> 
-    <BrowserRouter basename = "./portfolio/">
+    <BrowserRouter basename = "/portfolio/">
     <Routes>
       <Route index element={<Home/>}/> 
     </Routes>
